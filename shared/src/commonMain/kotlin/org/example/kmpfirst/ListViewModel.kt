67ce45157476e3b2f41367ae829f99a8ddb44310
Galyamin <63673861+imyounggod@users.noreply.github.com>
2024-08-11 21:10:28 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/org/example/kmpfirst/ListViewModel.kt
@@ -0,0 +1,11 @@
+package org.example.kmpfirst
+
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
+import com.rickclephas.kmp.observableviewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class ListViewModel : ViewModel() {
+    @NativeCoroutinesState
+    val objects: StateFlow<List<Int>> = MutableStateFlow(listOf(1,2,3))
+}
